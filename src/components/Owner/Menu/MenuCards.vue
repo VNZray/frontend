5dfe66895b17e21617f3b628ff40ee6dddbf7ae3
rayven_clores <rclores666@gmail.com>
2024-06-17 16:59:55 +0800

    remove save us function

--- src/components/Owner/Menu/MenuCards.vue
+++ src/components/Owner/Menu/MenuCards.vue
@@ -715,16 +715,6 @@ export default
               .catch(error => {
                 console.error('Error saving ingredients:', error);
               });
-
-            if (this.filename) {
-              const url = URL.createObjectURL(this.filename);
-              const link = document.createElement('a');
-              link.href = url;
-              link.download = this.filename.name;
-              document.body.appendChild(link);
-              link.click();
-              document.body.removeChild(link);
-            }
           })
           .catch(error => {
             console.error('Error saving menu item:', error);
